35c8688c02aa21fd9ddc7db646279ff780fe7485
rnstr <ranestarisastriani@gmail.com>
2021-11-19 09:18:42 +0700

    #add header
    added header and home

---
+++ src/header/Header.jsx
@@ -0,0 +1,13 @@
+import './header.css'
+
+export default function Header() {
+    return (
+        <div className="header">
+            <div className="headerTitles">
+                <span className="headerTitleSm">Ranestari Sastriani's</span>
+                <span className="headerTitleLg">Blog</span>
+            </div>
+            <img className="headerImg" src="https://cdn0-production-images-kly.akamaized.net/NvAoPNDx_gOVCCdyUXZDJarvkWw=/640x360/smart/filters:quality(75):strip_icc():format(jpeg)/kly-media-production/medias/3485446/original/011378800_1623921246-1.jpg" alt="" />
+        </div>
+    )
+}
